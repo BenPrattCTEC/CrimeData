28b64e147b0e0958d180d6728a88180e4111a399
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2018-01-31 11:40:43 -0700

    first commit

---
+++ src/Controller/Controller.cpp
@@ -0,0 +1,29 @@
+/*
+ * Controller.cpp
+ *
+ *  Created on: Jan 23, 2018
+ *      Author: ben
+ */
+
+#include "Controller.h"
+
+Controller::Controller() {
+	// TODO Auto-generated constructor stub
+
+}
+
+void Controller::start(){
+	ConsoleOutput console;
+	Arrays array;
+	VectorAssignment vectors;
+	console.start();
+	array.start();
+	vectors.start();
+
+
+}
+
+Controller::~Controller() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Controller/Controller.h
@@ -0,0 +1,25 @@
+/*
+ * Controller.h
+ *
+ *  Created on: Jan 23, 2018
+ *      Author: ben
+ */
+
+#ifndef SRC_CONTROLLER_CONTROLLER_H_
+#define SRC_CONTROLLER_CONTROLLER_H_
+
+#include <iostream>
+#include "../Model/ConsoleOutput.h"
+#include "../Model/Arrays.h"
+#include "../Model/VectorAssignment.h"
+
+using namespace std;
+
+class Controller {
+public:
+	Controller();
+	void start();
+	virtual ~Controller();
+};
+
+#endif /* SRC_CONTROLLER_CONTROLLER_H_ */
